385ec08c14999cdb5a2c112dec63afe7e3f1f46b
João José <joaogjose@outlook.com>
2024-08-29 17:34:28 +0100

    configurar o ambiente com conexão com base dados

---
+++ src/main/java/pt/com/transporteapi/transporteapi/TransporteApiApplication.java
@@ -0,0 +1,13 @@
+package pt.com.transporteapi.transporteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransporteApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransporteApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pt/com/transporteapi/transporteapi/TransporteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.com.transporteapi.transporteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransporteApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
